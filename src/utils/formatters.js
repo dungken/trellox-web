878cac0d52f27ef8a43e24a8b36a34b241138ef1
Van Dung Dev <vincentcode402112@gmail.com>
2025-02-08 13:55:41 +0700

    fix_bug_empty_column

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -3,3 +3,13 @@ export const capitalizeFirstLetter = (val) => {
   if (!val) return ''
   return `${val.charAt(0).toUpperCase()}${val.slice(1)}`
 }
+
+// Function to generate a placeholder card
+export const generatePlaceholderCard = (column) => {
+  return {
+    _id: `${column._id}-placeholder-card`,
+    boardId: column.boardId,
+    columnId: column._id,
+    FE_PlaceholderCard: true
+  }
+}
